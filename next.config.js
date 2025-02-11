6abb781e020dbe659b5b8292dcaf573c7e7440c8
Rostislav Egorov <rpegorov@icloud.com>
2025-02-11 13:58:47 +0300

    stupid recomndetion

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
-module.exports = {
-  reactStrictMode: true,
-  swcMinify: true,
+{
+  import('next').NextConfig
+}
+const nextConfig = {
   output: 'export',
   images: {
     unoptimized: true
   }
 }
+
+module.exports = nextConfig
