b307330b889fbdbb1b5fc3102827bf91c20b03dc
Rostislav Egorov <rpegorov@icloud.com>
2025-02-12 17:36:35 +0300

    static props

--- components/chakra.js
+++ components/chakra.js
@@ -18,10 +18,10 @@ export default function Chakra({ cookies, children }) {
   )
 }
 
-export async function getStaticProps({ req }) {
-  return {
-    props: {
-      cookies: req.headers.cookie ?? ''
-    }
-  }
-}
+// export async function getStaticProps({ req }) {
+//   return {
+//     props: {
+//       cookies: req.headers.cookie ?? ''
+//     }
+//   }
+// }
